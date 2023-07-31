418c18297ecfc313ee63dfb2baa21daee4c824a7
kimihiqq <kimihiqq@gmail.com>
2023-07-31 22:40:32 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/me/kimihiqq/tilstorage/TilStorageApplication.java
@@ -0,0 +1,13 @@
+package me.kimihiqq.tilstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilStorageApplication.class, args);
+    }
+
+}
